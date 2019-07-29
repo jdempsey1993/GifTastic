f2da1c10332472b22e27451111d507572237e6d2
Jack <jdempsey1993@gmail.com>
2019-07-29 16:42:07 -0400

    Inital HTML, JS Api function and AJAX Call

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 // Array for gif search items
 
 var topics = ["cat","dog","bird","snake","hamster","spider","turtle","guinea pig","pig","fish"]
+
+    //displayGIF function to display content within HTMl
+
+    function displayGIF () {
+        var gif =$(this).attr("data-name")
+        var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=s9PHSc6roItbCQkE3ggLzILRDRtK8U5D"
+    }
+    console.log(displayGIF)
+
+        //AJAX call for the buttons clicked
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+
+            // Div element to hold gif
+            var gifDiv = $("div class='gif'>")
+
+            //
