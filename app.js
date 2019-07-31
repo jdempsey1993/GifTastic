bcb4c59471d305abf155ee25a58d4fbafc6979f8
Jack <jdempsey1993@gmail.com>
2019-07-31 15:49:04 -0400

    Utilized JS for loop to generate array buttons, button CSS styling

--- app.js
+++ app.js
@@ -1,99 +1,112 @@
 
-//  Array for gif search items
+// //  First: Array for gif search items
 
 var topics = ["cat","dog","bird","snake","hamster","spider","turtle","guinea pig","pig","fish"]
 
-//Adding an onclick listener to buttons
-$("button").on("click", function()   {
-    
-    // Data animal property
-    
-    var animal = $(this).attr("data-animal")
+// 
 
-    //queryURL 
+function printBtn() {
+    for (var i = 0; i < topics.length; i++) {
+       var btn = document.createElement("button")
+       var t = document.createTextNode(topics[i])
+       btn.appendChild(t);
+       document.body.appendChild(btn);
+    }
+}
+
+printBtn();
+
+// //Adding an onclick listener to buttons
+// $("button").on("click", function()   {
     
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    animal + "&api_key=s9PHSc6roItbCQkE3ggLzILRDRtK8U5D&limit=10"
+//     // Data animal property
+    
+//     var animal = $(this).attr("data-animal")
+
+//     //queryURL 
+    
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//     animal + "&api_key=s9PHSc6roItbCQkE3ggLzILRDRtK8U5D&limit=10"
    
-    //AJAX call for the buttons clicked
+//     //AJAX call for the buttons clicked
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          })
+//         $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//           })
           
-          .then(function(response)     {
-              console.log(queryURL)
+//           .then(function(response)     {
+//               console.log(queryURL)
 
-              console.log(response)
+//               console.log(response)
 
-              //store AJAX request data
+//               //store AJAX request data
               
-              var results = response.data
+//               var results = response.data
 
-              // Loop for each result
+//               // Loop for each result
 
-              for (var i = 0; i <results.length; i++) {
+//               for (var i = 0; i <results.length; i++) {
 
-                // Creation of div tag
-                var animalDiv = $("<div>")
+//                 // Creation of div tag
+//                 var animalDiv = $("<div>")
 
-                //P tag creation for rating
+//                 //P tag creation for rating
 
-                var p = $("<p>").text("Rating: " + results[i].rating)
+//                 var p = $("<p>").text("Rating: " + results[i].rating)
                 
-                // Storing the image gif tag
+//                 // Storing the image gif tag
 
-                var animalImage = $("<img>")
+//                 var animalImage = $("<img>")
 
-                //Setting img src from result items
+//                 //Setting img src from result items
 
-                animalImage.attr("src,", results[i].images.fixed_height.url)
+//                 animalImage.attr("src,", results[i].images.fixed_height.url)
 
-                //Append paragraph and image tag to Div
+//                 //Append paragraph and image tag to Div
                 
-                animalDiv.append(p)
+//                 animalDiv.append(p)
 
-                animalDiv(animalImage)
+//                 animalDiv(animalImage)
 
-                //Preprend animalDiv to HTML
+//                 //Preprend animalDiv to HTML
 
-                $("#gifHere").prepend(animalDiv)
+//                 $("#gifHere").prepend(animalDiv)
 
-                //Jquery add class .gif to images 
+//                 //Jquery add class .gif to images 
 
-                $("<img>").addClass("gif")
+//                 $("<img>").addClass("gif")
 
 
             
-            }
+//             }
 
-          })
+//           })
 
-        })
+//         })
 
-        // Onclick element to pause gif
+//         // Onclick element to pause gif
 
-        $(".gif").on("click",function(){
+//         $(".gif").on("click",function(){
             
-            //Set value of data-animate
-            var state = $(this).attr("data-state")
+//             //Set value of data-animate
+//             var state = $(this).attr("data-state")
 
-            //If clicked switch between still and animate
+//             //If clicked switch between still and animate
 
-            if (state === "still") {
+//             if (state === "still") {
                 
-                $(this).attr("src",$(this).attr("data-animate"))
+//                 $(this).attr("src",$(this).attr("data-animate"))
 
-                $(this).attr("data-state","animate")
+//                 $(this).attr("data-state","animate")
 
-            } else {
+//             } else {
 
-                $(this).attr("src", $(this).attr("data-still"))
+//                 $(this).attr("src", $(this).attr("data-still"))
 
-                $(this).attr("data-state", "still")
-            }    
+//                 $(this).attr("data-state", "still")
+//             }    
         
-        }
-    )
+//         }
+//     )
 
